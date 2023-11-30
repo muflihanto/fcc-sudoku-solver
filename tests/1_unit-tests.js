0f9a9287d313b6978d7a2974ed70eaeb8ff14883
muflihanto <muflihanto@gmail.com>
2023-11-30 19:01:10 +0700

    add todo notes

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,20 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
+const Solver = require("../controllers/sudoku-solver.js");
 let solver;
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  // TODO: Logic handles a valid puzzle string of 81 characters
+  // TODO: Logic handles a puzzle string with invalid characters (not 1-9 or .)
+  // TODO: Logic handles a puzzle string that is not 81 characters in length
+  // TODO: Logic handles a valid row placement
+  // TODO: Logic handles an invalid row placement
+  // TODO: Logic handles a valid column placement
+  // TODO: Logic handles an invalid column placement
+  // TODO: Logic handles a valid region (3x3 grid) placement
+  // TODO: Logic handles an invalid region (3x3 grid) placement
+  // TODO: Valid puzzle strings pass the solver
+  // TODO: Invalid puzzle strings fail the solver
+  // TODO: Solver returns the expected solution for an incomplete puzzle
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,23 @@
 const chai = require("chai");
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {
-
+suite("Functional Tests", () => {
+  // TODO: Solve a puzzle with valid puzzle string: POST request to /api/solve
+  // TODO: Solve a puzzle with missing puzzle string: POST request to /api/solve
+  // TODO: Solve a puzzle with invalid characters: POST request to /api/solve
+  // TODO: Solve a puzzle with incorrect length: POST request to /api/solve
+  // TODO: Solve a puzzle that cannot be solved: POST request to /api/solve
+  // TODO: Check a puzzle placement with all fields: POST request to /api/check
+  // TODO: Check a puzzle placement with single placement conflict: POST request to /api/check
+  // TODO: Check a puzzle placement with multiple placement conflicts: POST request to /api/check
+  // TODO: Check a puzzle placement with all placement conflicts: POST request to /api/check
+  // TODO: Check a puzzle placement with missing required fields: POST request to /api/check
+  // TODO: Check a puzzle placement with invalid characters: POST request to /api/check
+  // TODO: Check a puzzle placement with incorrect length: POST request to /api/check
+  // TODO: Check a puzzle placement with invalid placement coordinate: POST request to /api/check
+  // TODO: Check a puzzle placement with invalid placement value: POST request to /api/check
 });
-
